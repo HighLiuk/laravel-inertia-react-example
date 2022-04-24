43cfa7a7fc476968064c1d09c99d7a4309bbf891
Luca Di Fazio <highliuk@gmail.com>
2022-04-24 14:55:36 +0200

    ♻ Routes Cleanup & UserController

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,10 @@
 <?php
 
-use App\Models\User;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return inertia('Home');
-});
+Route::inertia('/', 'Home');
 
-Route::get('/users', function () {
-    $filter = request('search');
-    $users = User::query()
-        ->when($filter, function ($query, $search) {
-            $query->where('name', 'like', "%$search%");
-        })
-        ->paginate(10, ['id', 'name'])
-        ->withQueryString();
+Route::resource('users', UserController::class)
+    ->only(['index', 'create']);
 
-
-    return inertia('Users/Index', compact('users', 'filter'));
-});
-
-Route::inertia('/users/create', 'Users/Create');
-
-Route::get('/settings', function () {
-    return inertia('Settings');
-});
-
-Route::post('/logout', function () {
-    dd(request('foo'));
-});
+Route::inertia('/settings', 'Settings');
